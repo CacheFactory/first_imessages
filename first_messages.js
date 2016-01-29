530bf81ee7c7f05807da27db43aaba80cf70a7dd
Edward Anderson <ejanderson4@gmail.com>
2016-01-29 12:49:25 -0800

    tie in contact names

--- first_messages.js
+++ first_messages.js
@@ -3,35 +3,79 @@ var fs = require("fs");
 var prompt = require('prompt');
 
 
-prompt.start();
-
 var dbFile = process.env.HOME+"/Library/Messages/chat.db";
+var contactDbFile = process.env.HOME+"/Library/Application Support/AddressBook/Sources/7DD08DF2-701B-4EDC-9FC5-B94C57231460/AddressBook-v22.abcddb";
 
-if(fs.existsSync(dbFile)){
-  var db = new sqlite3.Database(dbFile);
-
+var loadContacts = function(db, numberNameMap){
   var users = {};
+  numberNameMap = numberNameMap || {}
 
   db.all("SELECT ROWID, id From handle;", function(err, rows) {
     for(var i in rows){
-      users[rows[i].ROWID] = rows[i].id
-      console.log("ID: " + rows[i].ROWID + ": NUMBER: " + rows[i].id );
+      var cleanNumber = rows[i].id.replace(/\D/g,'');
+
+      var secondHalf = (numberNameMap[cleanNumber]) ? " Name: " + numberNameMap[cleanNumber] : " Number: " + cleanNumber
+
+      users[rows[i].ROWID] = cleanNumber
+      console.log("ID: " + rows[i].ROWID + secondHalf );
     }
 
-    console.log('User to load messages: ');
-
-    prompt.get(['id'], function (err, result) {
-      if (err) { return onErr(err); }
-      db.each("select * from message where handle_id=" + result.id + " ORDER BY ROWID LIMIT 20;", function(err, results) {
-        console.log(results.text)
-      });
-      
-
-    });
+    showMessages(db);
+    
   })
+}
+
+var showMessages = function(db){
+  console.log('User to load messages: ');
+  prompt.start();
+  prompt.get(['ID'], function (err, promptResult) {
+    if (err) { return onErr(err); }
+    db.each("select * from message where handle_id=" + promptResult.ID + " ORDER BY ROWID LIMIT 20;", function(err, result) {
+      if(result){
+        var prefix = result.is_from_me ? "You: " : "Other: " 
+        console.log(prefix + result.text);
+      }else{
+        console.log("No messages found")
+      }
+    });
+
+    showMessages(db)
+  });
+}
+
+var loadContactNames = function(db, callback){
+  db.all("select ZFULLNUMBER, ZFIRSTNAME, ZLASTNAME from ZABCDPHONENUMBER JOIN ZABCDRECORD ON ZABCDPHONENUMBER.ZOWNER = ZABCDRECORD.Z_PK;", function(err, rows) {
+    var numberNameMap = {}
+    
+    for(var i in rows){
+      var cleanNumber = rows[i].ZFULLNUMBER.replace(/\D/g,'');
+      if(cleanNumber.length == 10){
+        cleanNumber = "1" + cleanNumber
+      }
+      numberNameMap[cleanNumber] = (rows[i].ZFIRSTNAME || "") + (rows[i].ZLSTNAME || "");
+    }
+    callback(numberNameMap);
+    
+  })
+  
+}
+
+if(fs.existsSync(dbFile)){
+  var db = new sqlite3.Database(dbFile);
+  
+
+  if(fs.existsSync(contactDbFile)){
+    var contactDb = new sqlite3.Database(contactDbFile);
+    loadContactNames(contactDb, function(numberNameMap){
+      loadContacts(db, numberNameMap);
+    })
+  }else{
+    loadContacts(db);
+  }
+
+  
 
 }else{
   console.log("Message database does not exist");
 }
-
 
