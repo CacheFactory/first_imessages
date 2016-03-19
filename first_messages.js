ff26dbf8038a0b7f8151cb641b164cc60b8b2e5c
Edward Anderson <ejanderson4@gmail.com>
2016-03-18 18:25:18 -0700

    use promises

--- first_messages.js
+++ first_messages.js
@@ -1,16 +1,44 @@
 var sqlite3 = require('sqlite3').verbose();
 var fs = require("fs");
 var prompt = require('prompt');
+var glob = require('glob');
+var promise = require('promise');
+
+var pry = require('pryjs')
 
 
-var dbFile = process.env.HOME+"/Library/Messages/chat.db";
-var contactDbFile = process.env.HOME+"/Library/Application Support/AddressBook/Sources/7DD08DF2-701B-4EDC-9FC5-B94C57231460/AddressBook-v22.abcddb";
+var FirstMessages = function(chatDbFile, contactsDbFile){
+  this.chatDbFile = chatDbFile
+  this.contactsDbFile = contactsDbFile
+}
 
-var loadContacts = function(db, numberNameMap){
+FirstMessages.prototype.startCommandLine = function() {
+  var self = this;
+  if(fs.existsSync( this.chatDbFile) ){
+    var db = new sqlite3.Database(this.chatDbFile);
+    if(fs.existsSync(this.contactsDbFile)){ // load contacts if we have contacts file
+      var contactDb = new sqlite3.Database(this.contactsDbFile);
+      self.loadContactNames(contactDb).then(function(numberNameMap){
+        self.loadMessages(db, numberNameMap);
+      })
+    }else{
+      this.loadMessages(db);
+    }
+
+  }else{
+    console.log("Message database does not exist");
+  }
+
+  
+
+}
+
+FirstMessages.prototype.loadMessages = function(db, numberNameMap){
+  var self = this
   var users = {};
   numberNameMap = numberNameMap || {}
 
-  db.all("SELECT ROWID, id From handle;", function(err, rows) {
+  db.all("SELECT ROWID, id FROM `handle`;", function(err, rows) {
     for(var i in rows){
       var cleanNumber = rows[i].id.replace(/\D/g,'');
 
@@ -19,13 +47,13 @@ var loadContacts = function(db, numberNameMap){
       users[rows[i].ROWID] = cleanNumber
       console.log("ID: " + rows[i].ROWID + secondHalf );
     }
-
-    showMessages(db);
+    self.showMessages(db);
     
   })
 }
 
-var showMessages = function(db){
+FirstMessages.prototype.showMessages = function(db){
+  var self = this
   console.log('User to load messages: ');
   prompt.start();
   prompt.get(['ID'], function (err, promptResult) {
@@ -39,43 +67,53 @@ var showMessages = function(db){
       }
     });
 
-    showMessages(db)
+    self.showMessages(db)
   });
 }
 
-var loadContactNames = function(db, callback){
-  db.all("select ZFULLNUMBER, ZFIRSTNAME, ZLASTNAME from ZABCDPHONENUMBER JOIN ZABCDRECORD ON ZABCDPHONENUMBER.ZOWNER = ZABCDRECORD.Z_PK;", function(err, rows) {
-    var numberNameMap = {}
-    
-    for(var i in rows){
-      var cleanNumber = rows[i].ZFULLNUMBER.replace(/\D/g,'');
-      if(cleanNumber.length == 10){
-        cleanNumber = "1" + cleanNumber
+FirstMessages.prototype.loadContactNames = function(db){
+  var self = this
+  return new Promise(function (fulfill, reject){
+    db.all("select ZFULLNUMBER, ZFIRSTNAME, ZLASTNAME from ZABCDPHONENUMBER JOIN ZABCDRECORD ON ZABCDPHONENUMBER.ZOWNER = ZABCDRECORD.Z_PK;", function(err, rows) {
+      if(err){
+        reject(err)
       }
-      numberNameMap[cleanNumber] = (rows[i].ZFIRSTNAME || "") + (rows[i].ZLSTNAME || "");
-    }
-    callback(numberNameMap);
-    
-  })
-  
-}
-
-if(fs.existsSync(dbFile)){
-  var db = new sqlite3.Database(dbFile);
-  
-
-  if(fs.existsSync(contactDbFile)){
-    var contactDb = new sqlite3.Database(contactDbFile);
-    loadContactNames(contactDb, function(numberNameMap){
-      loadContacts(db, numberNameMap);
+      var numberNameMap = {}
+      for(var i in rows){
+        var cleanNumber = rows[i].ZFULLNUMBER.replace(/\D/g,'');
+        if(cleanNumber.length == 10){
+          cleanNumber = "1" + cleanNumber
+        }
+        numberNameMap[cleanNumber] = (rows[i].ZFIRSTNAME || "") + (rows[i].ZLSTNAME || "");
+      }
+      fulfill(numberNameMap);
+      
     })
-  }else{
-    loadContacts(db);
-  }
-
+  });
   
-
-}else{
-  console.log("Message database does not exist");
 }
+
+var getContactDBFile = function(){
+  return new Promise(function (fulfill, reject){
+    glob(process.env.HOME+"/Library/Application Support/AddressBook/Sources/**/*.abcddb", function (er, files) {
+      files.sort(function(file) {
+        var stats = fs.statSync(file)
+        return stats["size"]
+      })
+
+      fulfill(files[0]) // take the largest DB
+    }) 
+  })
+}
+
+var dbFile = process.env.HOME+"/Library/Messages/chat.db";
+
+getContactDBFile().then(function(chatDbFile){
+  var chat = new FirstMessages(dbFile,chatDbFile)
+  chat.startCommandLine()
+});
+
+
+
+
 
